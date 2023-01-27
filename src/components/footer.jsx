a1ddd3b19c9a2c16c7f573e969daa418f643082f
Sergio Inzunza <sinzunza@sdgku.edu>
2023-01-26 20:31:43 -0700

    107 #1

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <label>Sergio Inzunza</label>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        menu here
+    </div>
+  );
+}
+
+export default Navbar;
